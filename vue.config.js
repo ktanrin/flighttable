9081d89883a69b886669631712e720e8167f8c5b
ktanrin <144651151+ktanrin@users.noreply.github.com>
2023-12-31 12:58:12 +0700

    add icon

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        win: {
+          icon: 'src/assets/icon.png',
+          FontFace: 'src/assets/fonts/digital-7.ttf'
+        }
+      }
+    }
+    }
 })
